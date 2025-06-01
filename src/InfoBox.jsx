4f3599b4a6b5681ae73f32bc81aaf4208d6d4528
Aditya <aditya.031122@gmail.com>
2025-06-01 10:43:30 +0530

    weatherApp

---
+++ src/InfoBox.jsx
@@ -0,0 +1,41 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import SunnyIcon from '@mui/icons-material/Sunny';
+
+export default function InfoBox ({info}){
+  const INIT_URL = "https://images.unsplash.com/photo-1722858343990-1604f540c15d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww";
+  const HOT_URL = "https://images.unsplash.com/photo-1504370805625-d32c54b16100?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+  const COLD_URL = "https://images.unsplash.com/photo-1668531387310-9c3c2f272d52?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+  const RAIN_URL = "https://images.unsplash.com/photo-1558409057-bbe679023136?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFpbmluZ3xlbnwwfHwwfHx8MA%3D%3D";
+
+  return (
+    <div className="InfoBox">
+      <div className='cardContainer'>
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={info.humidity > 80 ? RAIN_URL: info.temp > 15 ? HOT_URL : COLD_URL}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city} {info.humidity > <ThunderstormIcon/> ? RAIN_URL: info.temp > 15 ? <SunnyIcon/> : <AcUnitIcon/> }
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+          <p>Temperature = {info.temp}&deg;C</p>
+          <p>Humidity = {info.humidity}</p>
+          <p>Min Temp = {info.tempMin}&deg;C </p>
+          <p>Max Temp = {info.tempMax}&deg;C  </p>
+          <p>The weather can be described as <i>{info.weather}</i> and feels like {info.feelslike}&deg;C </p>
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
+    </div>
+  )
+}
